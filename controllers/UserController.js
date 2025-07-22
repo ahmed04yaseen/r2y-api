cbcc8ef66933cf3e83a5144a1443b4048cd79059
ahmed04yaseen <ahmed04yaseen@gmail.com>
2025-07-22 15:05:29 +0530

    second commit

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -13,7 +13,7 @@ exports.registerNewUser = async (req, res, next) => {
 // Signup controller
 // controllers/authController.js
 // const generateOTP = () => Math.floor(1000 + Math.random() * 9000).toString(); // 4-digit OTP
-const generateOTP = "1234";
+const generateOTP = ()=>  "1234";
 exports.sendOTP = async (req, res, next) => {
   console.log(req.body)
   try {
